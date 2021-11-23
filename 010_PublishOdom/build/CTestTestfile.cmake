0a8fcdc2dce1b9b8f8b9f3664f5c900d8110e7ad
thiendong <thiendong.iuh@gmail.com>
2021-11-23 15:12:09 +0700

    add project 010_PublishOdomRos

---
+++ 010_PublishOdom/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/idea/Documents/CppLinux/010_PublishOdom
+# Build directory: /home/idea/Documents/CppLinux/010_PublishOdom/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ 010_PublishOdom/build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/idea/Documents/CppLinux/010_PublishOdom/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
